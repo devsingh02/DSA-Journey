a89fbb525850d8523357b059573ee172a53c30c7
Devansh Singh <468devsingh@gmail.com>
2022-12-27 20:20:40 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (88.81%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,21 +1,35 @@
-// New element forms pair with all previous same elements(count)
+// New element forms pair with all previous same elements(count == pairs of current num)
 
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>(); // <element, count>
-        int pairs = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i])) {
-                int count = hm.get(nums[i]);
-                pairs += count;   //pairs+=count (total current handshakes)
-                hm.put(nums[i], ++count);
-            }
-            else
-                hm.put(nums[i], 1);
+        HashMap<Integer, Integer> hm = new HashMap<>(); // <K=element, V=count>
+        int Total_pairs = 0;
+        for (int num : nums) {
+            int curr_count = hm.getOrDefault(num, 0); //return count if num present, else return 0
+            Total_pairs += curr_count;
+            hm.put(num, curr_count+1);
         }
-        return pairs;
+        return Total_pairs;
     }
 }
+
+//SOLUTION 1
+// class Solution {
+//     public int numIdenticalPairs(int[] nums) {
+//         HashMap<Integer, Integer> hm = new HashMap<>(); // <element, count>
+//         int pairs = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (hm.containsKey(nums[i])) {
+//                 int count = hm.get(nums[i]);
+//                 pairs += count;   //pairs+=count (total current handshakes)
+//                 hm.put(nums[i], ++count);
+//             }
+//             else
+//                 hm.put(nums[i], 1);
+//         }
+//         return pairs;
+//     }
+// }
 
 //SOLUTION 2
 // class Solution {
