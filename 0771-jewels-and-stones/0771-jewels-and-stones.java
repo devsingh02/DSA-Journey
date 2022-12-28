5133a40cb6917ab3ff69620b939d5cd14f42dbbc
Devansh Singh <468devsingh@gmail.com>
2022-12-28 12:25:58 +0530

    Time: 1 ms (95.89%), Space: 40.5 MB (93.16%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,26 +1,25 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        
-        HashMap<Character, Integer> J = new HashMap<>();    //only K imp.
-        for (int i = 0; i < jewels.length(); i++) J.put(jewels.charAt(i), 0); //input
+        HashSet<Character> J = new HashSet<>();
+        for (int i = 0; i < jewels.length(); i++) J.add(jewels.charAt(i)); //input
         int ans = 0;
         for (int i = 0; i < stones.length(); i++) {
             char ch = stones.charAt(i);
-            if (J.containsKey(ch)) ans++;
+            if (J.contains(ch)) ans++;
         }
         return ans;
     }
 }
 
+// USING HASHMAP
 // class Solution {
 //     public int numJewelsInStones(String jewels, String stones) {
-        
-//         HashSet<Character> J = new HashSet<>();
-//         for (int i = 0; i < jewels.length(); i++) J.add(jewels.charAt(i)); //input
+//         HashMap<Character, Integer> J = new HashMap<>();    //only K imp.
+//         for (int i = 0; i < jewels.length(); i++) J.put(jewels.charAt(i), 0); //input
 //         int ans = 0;
 //         for (int i = 0; i < stones.length(); i++) {
 //             char ch = stones.charAt(i);
-//             if (J.contains(ch)) ans++;
+//             if (J.containsKey(ch)) ans++;
 //         }
 //         return ans;
 //     }
