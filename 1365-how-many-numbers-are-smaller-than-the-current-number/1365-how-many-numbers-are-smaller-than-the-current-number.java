c243c08ac5dd3442ca150981b54bc49850875cac
Devansh Singh <468devsingh@gmail.com>
2022-12-28 19:17:19 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (75.36%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,20 +1,43 @@
-// HASHMAP
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>(); // <K=element, V=1st_ Index>
-        int[] sort = nums.clone(); Arrays.sort(sort);
-        // int[] count = new int[nums.length];
+        int[] count = new int[101];
+        int[] res = new int[nums.length];
         
-        for (int i = 0; i < sort.length; i++) {
-            hm.putIfAbsent(sort[i], i);
+        for (int i =0; i < nums.length; i++) {
+            count[nums[i]]++;
         }
+        
+        for (int i = 1 ; i <= 100; i++) {
+            count[i] += count[i-1];    
+        }
+        
         for (int i = 0; i < nums.length; i++) {
-            // count[i] = hm.get(nums[i]);
-            sort[i] = hm.get(nums[i]);
+            if (nums[i] == 0)
+                res[i] = 0;
+            else 
+                res[i] = count[nums[i] - 1];
         }
-        return sort;
+        
+        return res;        
     }
 }
+
+// HASHMAP
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         HashMap<Integer, Integer> hm = new HashMap<>(); // <K=element, V=1st_ Index>
+//         int[] sort = nums.clone(); Arrays.sort(sort);
+//         int[] count = new int[nums.length];
+        
+//         for (int i = 0; i < sort.length; i++) {
+//             hm.putIfAbsent(sort[i], i);
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             count[i] = hm.get(nums[i]);
+//         }
+//         return count;
+//     }
+// }
 
 //  BRUTE FORCE  O(n*n)
 // class Solution {
