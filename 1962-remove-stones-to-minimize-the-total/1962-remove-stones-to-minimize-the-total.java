4318b5ef913502a27d9eafbb1dc6855033dd73bb
Devansh Singh <468devsingh@gmail.com>
2022-12-31 23:10:08 +0530

    Time: 524 ms (59.94%), Space: 55.5 MB (72.37%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -20,3 +20,50 @@ class Solution {
         return sum;
     }
 }
+
+
+// BRUTE FORCE 1 : Time Limit Exceeded  O(k*n)
+// class Solution {
+//     public int minStoneSum(int[] piles, int k) {
+//         if (k == 0) {
+//             int sum = 0;
+//             for (int I : piles) sum += I;
+//             return sum;
+//         }
+//         int max_ind = 0;
+//         for (int i = 0; i < piles.length; i++) {
+//             if (piles[i] > piles[max_ind]) max_ind = i;
+//         }
+//         piles[max_ind] -= (int)(piles[max_ind]/2);
+//         return minStoneSum(piles, --k);
+//     }
+// }
+
+// BRUTE FORCE 2 : Time Limit Exceeded  O(k*nlogn)
+// class Solution {
+//     public int minStoneSum(int[] piles, int k) {
+//         if (k == 0) {
+//             int sum = 0;
+//             for (int I : piles) sum += I;
+//             return sum;
+//         }
+//         Arrays.sort(piles);
+//         int n = piles.length;
+//         piles[n-1] -= (int)(piles[n-1]/2);
+//         return minStoneSum(piles, --k);
+//     }
+// }
+
+// ALTERNATE Q. (RANDOM ELEMENT)
+// class Solution {
+//     public int minStoneSum(int[] piles, int k) {
+//         if (k == 0) {
+//             int sum = 0;
+//             for (int I : piles) sum += I;
+//             return sum;
+//         }
+//         int rand = ThreadLocalRandom.current().nextInt(0, piles.length);
+//         piles[rand] -= (int)(piles[rand]/2);
+//         return minStoneSum(piles, --k);
+//     }
+// }
