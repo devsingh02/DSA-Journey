22581f5e40c0e1433f862ffd91944846cc6b7081
Devansh Singh <468devsingh@gmail.com>
2022-12-31 23:11:49 +0530

    Time: 495 ms (67.03%), Space: 58.7 MB (7.31%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,41 +1,26 @@
 // piles[] -> no. of stones in ith pile
 
+
 class Solution {
-    public static int minStoneSum(int[] piles, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+    public int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+         
         int sum = 0;
-        for(int p: piles){
-            pq.add(p);
-            sum += p;
+        for (int I : piles) {       //n
+            pq.offer(I);    //logn
+            sum += I;   // maintain sum from start to avoid addtional O(nlogn) in end
         }
-        while(k-->0){
-            int num = pq.poll();
-            pq.add(num - num / 2);
-            sum -= num/2;
+        
+        while (k > 0) {
+            int max = pq.poll(); //removed the element
+            int new_element = max - (int)max/2 ;
+            sum -= (int)max/2;
+            pq.offer(new_element);
+            k--;
         }
         return sum;
     }
 }
-// class Solution {
-//     public int minStoneSum(int[] piles, int k) {
-//         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-         
-//         int sum = 0;
-//         for (int I : piles) {       //n
-//             pq.offer(I);    //logn
-//             sum += I;   // maintain sum from start to avoid addtional O(nlogn) in end
-//         }
-        
-//         while (k > 0) {
-//             int max = pq.poll(); //removed the element
-//             int new_element = max - (int)max/2 ;
-//             sum -= (int)max/2;
-//             pq.offer(new_element);
-//             k--;
-//         }
-//         return sum;
-//     }
-// }
 
 
 // BRUTE FORCE 1 : Time Limit Exceeded  O(k*n)
