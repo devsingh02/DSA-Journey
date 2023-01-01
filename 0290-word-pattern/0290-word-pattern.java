cff2dd51e837071a0778165de2fcea82d6392827
Devansh Singh <468devsingh@gmail.com>
2023-01-01 19:07:40 +0530

    Time: 1 ms (95.36%), Space: 40.1 MB (90.03%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,56 +1,42 @@
+
+
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String [] arr = s.split(" ");
-        if(pattern.length()!=arr.length)return false;
-        HashMap<Character,String> map = new HashMap<>(); 
-        for(int i = 0; i<pattern.length(); i++){
+        
+        HashMap<String, Character> hm = new HashMap<>();
+        
+        // Checking if pattern.length = no. of words
+        int ct = 0; s = s + " ";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)==' ') ct++;
+        }
+        if (ct != pattern.length()) return false;        
+        
+        
+        int start = 0, end;
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            
+            // if (start >= s.length()) return false;
+            end = s.indexOf(' ', start);
+            String word = s.substring(start, end);
+            start = end + 1;
+            
+            // if (i==pattern.length()-1 && start<s.length()) return false;
             char ch = pattern.charAt(i);
-            boolean containsKey = map.containsKey(ch);
-            if(map.containsValue(arr[i]) && !containsKey)return false;
-            if(containsKey && !map.get(ch).equals(arr[i]))return false;
-            else map.put(ch,arr[i]);
+            
+            if (hm.containsKey(word) && hm.get(word)==ch)
+                continue;
+            if (!hm.containsKey(word) && !hm.containsValue(ch)) {
+                hm.put(word, ch);
+                continue;
+            }
+            return false;
         }
         return true;
     }
 }
-
-
-// class Solution {
-//     public boolean wordPattern(String pattern, String s) {
-        
-//         HashMap<String, Character> hm = new HashMap<>();
-        
-//         // Checking if pattern.length = no. of words
-//         int ct = 0; s = s + " ";
-//         for (int i = 0; i < s.length(); i++) {
-//             if (s.charAt(i)==' ') ct++;
-//         }
-//         if (ct != pattern.length()) return false;        
-        
-        
-//         int start = 0, end;
-        
-//         for (int i = 0; i < pattern.length(); i++) {
-            
-//             // if (start >= s.length()) return false;
-//             end = s.indexOf(' ', start);
-//             String word = s.substring(start, end);
-//             start = end + 1;
-            
-//             // if (i==pattern.length()-1 && start<s.length()) return false;
-//             char ch = pattern.charAt(i);
-            
-//             if (hm.containsKey(word) && hm.get(word)==ch)
-//                 continue;
-//             if (!hm.containsKey(word) && !hm.containsValue(ch)) {
-//                 hm.put(word, ch);
-//                 continue;
-//             }
-//             return false;
-//         }
-//         return true;
-//     }
-// }
 
 
 // class Solution {
