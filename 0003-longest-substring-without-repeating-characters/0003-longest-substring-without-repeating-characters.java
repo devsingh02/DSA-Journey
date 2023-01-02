1fa957a3f275f9c0ef0d53209c8a8846cd384725
Devansh Singh <468devsingh@gmail.com>
2023-01-02 20:57:24 +0530

    Time: 5 ms (94.50%), Space: 42.6 MB (80.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,33 +1,61 @@
-// O(n*n)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> hs = new HashSet<>();
-        String max = "";
-        for (int i = 0; i < s.length(); i++) {
-            String str = "";
-            for (int j = i; j < s.length(); j++) {
-                char ch = s.charAt(j);
-                if (hs.contains(ch)) {
-                    if (str.length() > max.length())
-                        max = str;
-                    break;
-                }
-                else if (j+1 == s.length()) {
-                    str = str + ch;
-                    if (str.length() > max.length())
-                        max = str;
-                    hs.add(ch);
-                }
-                else {
-                    str = str + ch;
-                    hs.add(ch);
-                }
-            }
-            hs.clear();
+        if (s == null) {
+            throw new IllegalArgumentException("Input string is null");
         }
-        return max.length();
+
+        int len = s.length();
+        if (len <= 1) {
+            return len;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int maxLen = 0;
+
+        for (int end = 0; end < len; end++) {
+            char eChar = s.charAt(end);
+            if (map.containsKey(eChar)) {
+                start = Math.max(start, map.get(eChar) + 1);
+            }
+            map.put(eChar, end);
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+
+        return maxLen;
     }
 }
+
+// O(n*n)
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         HashSet<Character> hs = new HashSet<>();
+//         String max = "";
+//         for (int i = 0; i < s.length(); i++) {
+//             String str = "";
+//             for (int j = i; j < s.length(); j++) {
+//                 char ch = s.charAt(j);
+//                 if (hs.contains(ch)) {
+//                     if (str.length() > max.length())
+//                         max = str;
+//                     break;
+//                 }
+//                 else if (j+1 == s.length()) {
+//                     str = str + ch;
+//                     if (str.length() > max.length())
+//                         max = str;
+//                     hs.add(ch);
+//                 }
+//                 else {
+//                     str = str + ch;
+//                     hs.add(ch);
+//                 }
+//             }
+//             hs.clear();
+//         }
+//         return max.length();
+//     }
+// }
 
 
 // WITH SAKSHAM
