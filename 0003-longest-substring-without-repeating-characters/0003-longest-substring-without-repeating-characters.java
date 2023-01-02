aa878256c81bef529d4a3540dfabc1c9bbe75965
Devansh Singh <468devsingh@gmail.com>
2023-01-02 21:01:58 +0530

    Time: 2 ms (99.99%), Space: 42.5 MB (80.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,35 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int i = 0;
-        int j = 0;
-        int max = 0;
-        while(j < s.length()){
-            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
-            if(map.size() == j - i + 1){
-                max = Math.max(max, j - i + 1);
-                j++;
-            }
-            else if(map.size() < j - i + 1){
-                while(map.size() < j - i + 1){
-                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-                    if(map.get(s.charAt(i)) == 0) map.remove(s.charAt(i));
-                    i++;
-                }
-                j++;
-            }
+class Solution 
+{
+    public int lengthOfLongestSubstring(String s) 
+    {
+        int maxLen = 0;
+        
+        // [1] longest substring is the one with the largest 
+        //    difference between positions of repeated characters; 
+        //    thus, we should create a storage for such positions 
+        int[] pos = new int[128];
+
+        // [2] while iterating through the string (i.e., moving 
+        //    the end of the sliding window), we should also 
+        //    update the start of the window 
+        int start = 0, end = 0;
+
+        for (char ch : s.toCharArray())
+        {
+            // [3] get the position for the start of sliding window 
+            //    with no other occurences of 'ch' in it 
+            start  = Math.max(start, pos[ch]);
+
+            // [4] update maximum length 
+            maxLen = Math.max(maxLen, end-start+1);
+
+            // [5] set the position to be used in [3] on next iterations
+            pos[ch] = end + 1;
+            
+            end++;
         }
-        return max;
+
+        return maxLen;
     }
 }
 
