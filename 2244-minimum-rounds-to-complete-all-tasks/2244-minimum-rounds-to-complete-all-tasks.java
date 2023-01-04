b791925c601f777182e292efcc62018fd3efe05b
Devansh Singh <468devsingh@gmail.com>
2023-01-04 17:00:10 +0530

    Time: 22 ms (94.43%), Space: 83.3 MB (62.44%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,21 +1,50 @@
 // 1 round -> 2/3 same level tasks
 class Solution {
     public int minimumRounds(int[] tasks) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int I : tasks) {
-            hm.put(I, hm.getOrDefault(I, 0) + 1);
+        // Sort the tasks array in ascending order
+        Arrays.sort(tasks);
+        int result = 0, count = 0;
+        // Iterate through each task
+        for (int i = 0; i < tasks.length; i++) {
+            // Increment the count of tasks with the same value
+            count++;
+            // If we have reached the last task or the current task is different from the next task
+            if (i == tasks.length - 1 || tasks[i] != tasks[i + 1]) {
+                // If there is only one task with this value, we cannot create groups of 3
+                // and we return -1
+                if (count == 1) {
+                    return -1;
+                }
+                // Add the number of full groups of 3 we can create from the tasks with this value
+                result += count / 3;
+                // If there are remaining tasks that cannot be included in a group of 3,
+                // we need one more round to process these tasks
+                if(count % 3 != 0) result++;
+                // Reset the count for the next set of tasks
+                count = 0;
+           }
         }
-        int rnds = 0;
-        for (int ct : hm.values()) {
-            if (ct == 1) return -1;
-            
-            if (ct%3==0) rnds += ct/3;
-            else if (ct%3==1) rnds += ct/3-1 + 2;
-            else rnds += ct/3 + 1;
-        }
-        return rnds;
+        // Return the total number of rounds needed
+        return result;
     }
 }
+// class Solution {
+//     public int minimumRounds(int[] tasks) {
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         for (int I : tasks) {
+//             hm.put(I, hm.getOrDefault(I, 0) + 1);
+//         }
+//         int rnds = 0;
+//         for (int ct : hm.values()) {
+//             if (ct == 1) return -1;
+            
+//             if (ct%3==0) rnds += ct/3;
+//             else if (ct%3==1) rnds += ct/3-1 + 2;
+//             else rnds += ct/3 + 1;
+//         }
+//         return rnds;
+//     }
+// }
     
 // HashMap    
 // class Solution {
