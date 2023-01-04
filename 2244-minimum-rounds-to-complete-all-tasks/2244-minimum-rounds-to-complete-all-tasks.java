c848cdfb2af2045477dfc8aaba356a33b6b0e805
Devansh Singh <468devsingh@gmail.com>
2023-01-04 19:15:19 +0530

    Time: 14 ms (95.83%), Space: 84.7 MB (60.35%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,57 +1,38 @@
 // 1 round -> 2/3 same level tasks
 
-class Solution {
-    public int minimumRounds(int[] tasks) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int I : tasks) {
-            hm.put(I, hm.getOrDefault(I, 0) + 1);
-        }
-        int rnds = 0;
-        for (int ct : hm.values()) {
-            if (ct == 1) return -1;
-            
-            // if (ct%3==0) rnds += ct/3;
-            // else if (ct%3==1) rnds += ct/3-1 + 2;
-            // else rnds += ct/3 + 1;
-            rnds += ct/3;
-            if(ct%3!=0) rnds++;
-        }
-        return rnds;
-    }
-}
-    
-// HashMap    
+// HashMap 
 // class Solution {
 //     public int minimumRounds(int[] tasks) {
-//         HashMap<Integer, Integer> hm = new HashMap<>(); // <task-level,its-count> 
+//         HashMap<Integer, Integer> hm = new HashMap<>();
 //         for (int I : tasks) {
-//             if (!hm.containsKey(I))
-//                 hm.put(I, 1);
-//             else {
-//                 int v = hm.get(I);
-//                 v++;
-//                 hm.put(I, v);
-//             } 
+//             hm.put(I, hm.getOrDefault(I, 0) + 1);
 //         }
-//         int tot_rounds = 0;
-//         for (Map.Entry<Integer, Integer> e : hm.entrySet()) {
-//             int ct = e.getValue();
-//             int r = Rounds(ct);
-//             if (r==-1) return -1;
-//             tot_rounds += r;
+//         int rnds = 0;
+//         for (int ct : hm.values()) {
+//             if (ct == 1) return -1;
+            
+//             // if (ct%3==0) rnds += ct/3;
+//             // else if (ct%3==1) rnds += ct/3-1 + 2;
+//             // else rnds += ct/3 + 1;
+//             rnds += ct/3;
+//             if(ct%3!=0) rnds++;
 //         }
-//         return tot_rounds;
-//     }
-//     public int Rounds(int n) {
-//         if (n==1) return -1;
-        
-//         if (n%3==0) 
-//             return n/3;
-//         if (n%3==2)
-//             return (int)n/3 + 1;
-//         if (n%3==1)
-//             return (int)n/3-1 + 2;
-        
-//         return 0;
+//         return rnds;
 //     }
 // }
+    
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        Arrays.sort(tasks);
+        int res = 0;
+        int i=0;
+        while(i<tasks.length) {
+            int j=i+1;
+            while(j<tasks.length && tasks[i] == tasks[j]) j++;
+            if(j-i==1) return -1;
+            res += (j-i)%3 == 0 ? (j-i)/3 : (j-i)/3 + 1;
+            i = j;
+        }
+        return res;
+    }
+}   
