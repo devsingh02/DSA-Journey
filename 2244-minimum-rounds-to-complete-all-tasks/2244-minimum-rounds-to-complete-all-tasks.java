1712d70a6c482cb26a436ecb31b8bff331948a99
Devansh Singh <468devsingh@gmail.com>
2023-01-04 16:57:32 +0530

    Time: 98 ms (25.04%), Space: 107.8 MB (19.83%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,38 +1,19 @@
 // 1 round -> 2/3 same level tasks
-// COUNTING / BUCKET
 class Solution {
-    // This function returns the minimum number of rounds needed to
-    // process all tasks in the given array of tasks.
     public int minimumRounds(int[] tasks) {
-
-        // Create a HashMap to store the count of each task
-        HashMap<Integer, Integer> getCount = new HashMap<>();
-
-        // Iterate through the tasks array and store the count of each task
-        // in the HashMap
-        for (int t : tasks) {
-            getCount.put(t, getCount.getOrDefault(t, 0) + 1);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int I : tasks) {
+            hm.put(I, hm.getOrDefault(I, 0) + 1);
         }
-
-        // Initialize a result variable to 0
-        int result = 0;
-
-        // Iterate through the values in the HashMap (which represent the counts of the tasks)
-        for (int count : getCount.values()) {
-            // If a task has a count of 1, it cannot be grouped with other tasks
-            // and so we return -1
-            if (count == 1) return -1;
-
-            // Add the number of rounds needed to process the tasks with count greater than or equal to 3
-            result += count / 3;
-
-            // If there are any tasks left with count less than 3, we need an additional round
-            // to process these tasks
-            if(count % 3 != 0) result++;
+        int rnds = 0;
+        for (int ct : hm.values()) {
+            if (ct == 1) return -1;
+            
+            if (ct%3==0) rnds += ct/3;
+            else if (ct%3==1) rnds += ct/3-1 + 2;
+            else rnds += ct/3 + 1;
         }
-
-        // Return the total number of rounds needed to process all tasks
-        return result;
+        return rnds;
     }
 }
     
