96dae40f443122e1a6ade9fd1dbcfd11bb90a855
Devansh Singh <468devsingh@gmail.com>
2023-01-04 19:17:32 +0530

    Time: 99 ms (23.48%), Space: 105.4 MB (32.18%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -21,18 +21,59 @@
 //     }
 // }
     
+// class Solution {
+//     public int minimumRounds(int[] tasks) {
+//         Arrays.sort(tasks);
+//         int res = 0;
+//         int i=0;
+//         while(i<tasks.length) {
+//             int j=i+1;
+//             while(j<tasks.length && tasks[i] == tasks[j]) j++;
+//             if(j-i==1) return -1;
+//             res += (j-i)%3 == 0 ? (j-i)/3 : (j-i)/3 + 1;
+//             i = j;
+//         }
+//         return res;
+//     }
+// } 
 class Solution {
     public int minimumRounds(int[] tasks) {
-        Arrays.sort(tasks);
-        int res = 0;
-        int i=0;
-        while(i<tasks.length) {
-            int j=i+1;
-            while(j<tasks.length && tasks[i] == tasks[j]) j++;
-            if(j-i==1) return -1;
-            res += (j-i)%3 == 0 ? (j-i)/3 : (j-i)/3 + 1;
-            i = j;
+        // Put the task numbers and their counts
+        HashMap<Integer, Integer> taskMap = new HashMap<>();
+        for(int task: tasks){
+            taskMap.put(task, taskMap.getOrDefault(task, 0)+1);
         }
-        return res;
+        
+        // Count the number of rounds
+        int rounds = 0;
+        for(int key:taskMap.keySet()){            
+            // Check if only 1 task at a time is possible
+            int count = taskMap.get(key);
+            if(count == 1){
+                return -1;
+            }
+            
+            // Round counter for this specific task number
+            int roundTmp = 0;
+            roundTmp = (int) count / 3;
+            int remainder = count % 3;
+            
+            // Switch statement can obviously be simplified
+            switch(remainder){
+                // No remainder, perfect division by 3
+                case 0:
+                    rounds += roundTmp;
+                    break;
+                // Remainder of 1, too many 3s. Lose 1 three tasks and add 2 two tasks
+                case 1:
+                    rounds += roundTmp + 1;
+                    break;
+                // Remainder of 2, not enough, add 1 two tasks.
+                case 2:
+                    rounds += roundTmp + 1;
+                    break;                    
+            }
+        }
+        return rounds;
     }
-}   
+}
