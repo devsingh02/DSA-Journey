3d14881df58971efcd1eb170c23bacfa0da7d351
Devansh Singh <468devsingh@gmail.com>
2023-01-05 14:37:21 +0530

    Time: 125 ms (35.63%), Space: 96.5 MB (35.45%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,33 +1,24 @@
 class Solution {
-    public int findMinArrowShots(int[][] points) {
-        // result will store the minimum number of arrows needed to burst all the balloons
-        int result = 0;
-
-        // PriorityQueue to sort the balloon ranges in ascending order of their end points
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-
-        // add all the balloon ranges to the PriorityQueue
-        for (int []point : points) priorityQueue.add(point);
-
-        // if there are no balloons, return 0
-        if (priorityQueue.isEmpty()) return 0;
-
-        // while there are balloons in the PriorityQueue, process each balloon range
-        while (!priorityQueue.isEmpty()) {
-            // get the current balloon range
-            int [] prev = priorityQueue.poll();
-
-            // check if any subsequent balloon ranges start before or at the same time as the end of the current range
-            // if a subsequent range does, remove it from the PriorityQueue because it will also be burst when the current range is burst
-            while (!priorityQueue.isEmpty() && prev[1]>=priorityQueue.peek()[0] && prev[1]<=priorityQueue.peek()[1]) {
-                priorityQueue.poll();
+   // Finds the minimum number of arrows needed to burst all balloons
+   public int findMinArrowShots(int[][] points) {
+        // Sort the balloons by the end position of the arrow
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1],b[1]));
+        
+        // Initialize the number of arrows needed to 1
+        int result = 1;
+        // Set the index of the previously burst balloon to 0
+        int previous = 0;
+        
+        // Iterate through all balloons
+        for(int current = 1; current < points.length; current++){
+            // If the start position of the current balloon is after the end position of the arrow that burst the previous balloon,
+            // then a new arrow is needed to burst the current balloon
+            if(points[current][0] > points[previous][1]){
+                result++;
+                // Update the index of the previously burst balloon
+                previous = current;
             }
-
-            // increment the result counter by 1 for each balloon range processed, since each range requires one arrow to burst it
-            result++;
         }
-
-        // return the minimum number of arrows needed to burst all the balloons
-        return result;
+        return result;            
     }
 }
