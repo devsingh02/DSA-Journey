49c9133232ab710fb964fb993024e68bc93c61a5
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-08 10:43:17 +0530

    Time: 7 ms (9.03%), Space: 44 MB (45.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -5,17 +5,32 @@ class Solution {
         helper(0, 0, candidates, target, curset, subset);
         return subset;
     }
-    static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
+    static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {//take "i"
         if (sum == target) {
             subset.add(new ArrayList<>(curset));
             return;
         }
-        if (sum > target) return;
-        
-        for (int j = i; j < candidates.length; j++) {
-            curset.push(candidates[j]); sum += candidates[j];
-            helper(j, sum, candidates, target, curset, subset);
-            sum -= curset.pop(); //sum + element removed
-        }
+        if (sum > target || i == candidates.length) return;
+
+        //choice -> take i
+        curset.push(candidates[i]); sum+=candidates[i];// TAKE "i"
+        helper(i , sum, candidates, target, curset, subset); //all possible combinations with "i"
+        sum-=curset.pop();
+        //choice -> don't take
+        helper(i + 1, sum, candidates, target, curset, subset);
     }
 }
+//     static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
+//         if (sum == target) {
+//             subset.add(new ArrayList<>(curset));
+//             return;
+//         }
+//         if (sum > target) return;
+        
+//         for (int j = i; j < candidates.length; j++) {
+//             curset.push(candidates[j]); sum += candidates[j];
+//             helper(j, sum, candidates, target, curset, subset);
+//             sum -= curset.pop(); //sum + element removed
+//         }
+//     }
+// }
