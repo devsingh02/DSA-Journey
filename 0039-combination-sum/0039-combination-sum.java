a267999591f6906683dc2e135ebad8003968624d
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-08 10:29:32 +0530

    Time: 7 ms (9.03%), Space: 43.8 MB (68.54%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> subset = new ArrayList<>();
+        Stack<Integer> curset = new Stack<>();
+        helper(0, 0, candidates, target, curset, subset);
+        return subset;
+    }
+    static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
+        if (sum == target) {
+            subset.add(new ArrayList<>(curset));
+            return;
+        }
+        if (sum > target) return;
+        
+        for (int j = i; j < candidates.length; j++) {
+            curset.push(candidates[j]); sum += candidates[j];
+            helper(j, sum, candidates, target, curset, subset);
+            sum -= curset.pop(); //sum + element removed
+        }
+    }
+}
