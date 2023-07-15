c578e8e4eb3ffd4f818a8402a5f9c8f36b21936b
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-15 12:04:56 +0530

    Time: 3 ms (39.58%), Space: 43.9 MB (64.83%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,53 +1,53 @@
-// class Solution {
-//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//         List<List<Integer>> ans = new ArrayList<>();
-//         findCombinations(0, candidates, target, ans, new Stack<>());
-//         return ans;
-//     }
-
-//     private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, Stack<Integer> ds) {
-//         if (target == 0) {
-//             ans.add(new ArrayList<>(ds));
-//             return;
-//         }
-
-//         if (ind == arr.length || target < 0) {
-//             return;
-//         }
-
-//         // Choice 1: Take the current element
-//         if (arr[ind] <= target) {
-//             ds.push(arr[ind]);
-//             findCombinations(ind, arr, target - arr[ind], ans, ds);
-//             ds.pop();
-//         }
-
-//         // Choice 2: Don't take the current element
-//         findCombinations(ind + 1, arr, target, ans, ds);
-//     }
-// }
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> subsets = new ArrayList<>();
-        backtrack(0, target, candidates, new ArrayList<>(), subsets);
-        return subsets;
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(0, candidates, target, ans, new Stack<>());
+        return ans;
     }
 
-    private void backtrack(int index, int target, int[] candidates, List<Integer> currentSubset, List<List<Integer>> subsets) {
+    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, Stack<Integer> ds) {
         if (target == 0) {
-            subsets.add(new ArrayList<>(currentSubset));
+            ans.add(new ArrayList<>(ds));
             return;
         }
 
-        for (int i = index; i < candidates.length; i++) {
-            if (candidates[i] <= target) {
-                currentSubset.add(candidates[i]);
-                backtrack(i, target - candidates[i], candidates, currentSubset, subsets);
-                currentSubset.remove(currentSubset.size() - 1);
-            }
+        if (ind == arr.length || target < 0) {
+            return;
         }
+
+        // Choice 1: Take the current element
+        if (arr[ind] <= target) {
+            ds.push(arr[ind]);
+            findCombinations(ind, arr, target - arr[ind], ans, ds);
+            ds.pop();
+        }
+
+        // Choice 2: Don't take the current element
+        findCombinations(ind + 1, arr, target, ans, ds);
     }
 }
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> subsets = new ArrayList<>();
+//         backtrack(0, target, candidates, new ArrayList<>(), subsets);
+//         return subsets;
+//     }
+
+//     private void backtrack(int index, int target, int[] candidates, List<Integer> currentSubset, List<List<Integer>> subsets) {
+//         if (target == 0) {
+//             subsets.add(new ArrayList<>(currentSubset));
+//             return;
+//         }
+
+//         for (int i = index; i < candidates.length; i++) {
+//             if (candidates[i] <= target) {
+//                 currentSubset.add(candidates[i]);
+//                 backtrack(i, target - candidates[i], candidates, currentSubset, subsets);
+//                 currentSubset.remove(currentSubset.size() - 1);
+//             }
+//         }
+//     }
+// }
 
 
 // // MEHTOD 1 : SubSequence 
