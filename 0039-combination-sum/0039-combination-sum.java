292519e65a04af80eda0fdd729eb859cc812811b
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-15 10:58:14 +0530

    Time: 8 ms (7.96%), Space: 43.8 MB (83.37%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,53 +2,19 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> subset = new ArrayList<>();
         Stack<Integer> curset = new Stack<>();
-        helper(0, candidates, target, curset, subset);
+        helper(0, target, candidates, curset, subset);
         return subset;
     }
-        static void helper(int i, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
-        if (target == 0) {
-            subset.add(new ArrayList<>(curset));
+    static void helper(int i, int target, int[] candidates, Stack<Integer> curset, List<List<Integer>> subset) {
+        if (i == candidates.length || target < 0) {
+            if (target == 0) subset.add(new ArrayList<>(curset));
             return;
         }
-        if (target < 0) return;
-        
-        for (int j = i; j < candidates.length; j++) {
-            curset.push(candidates[j]); target -= candidates[j];
-            helper(j, candidates, target, curset, subset);
-            target += curset.pop(); //sum + element removed
-        }
+        // choice 1 : Take
+        curset.push(candidates[i]);
+        helper(i, target - candidates[i], candidates, curset, subset);
+        //choice 2 : Don't Take
+        curset.pop();
+        helper(i + 1, target, candidates, curset, subset);
     }
 }
-    
-    // METHOD 1 :
-//     static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {//take "i"
-//         if (sum == target) {
-//             subset.add(new ArrayList<>(curset));
-//             return;
-//         }
-//         if (sum > target || i == candidates.length) return;
-
-//         //choice -> take i
-//         curset.push(candidates[i]); sum+=candidates[i];
-//         helper(i , sum, candidates, target, curset, subset); 
-//         sum-=curset.pop();
-//         //choice -> don't take
-//         helper(i + 1, sum, candidates, target, curset, subset);
-//     }
-// }
-
-// METHOD 2
-//     static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
-//         if (sum == target) {
-//             subset.add(new ArrayList<>(curset));
-//             return;
-//         }
-//         if (sum > target) return;
-        
-//         for (int j = i; j < candidates.length; j++) {
-//             curset.push(candidates[j]); sum += candidates[j];
-//             helper(j, sum, candidates, target, curset, subset);
-//             sum -= curset.pop(); //sum + element removed
-//         }
-//     }
-// }
