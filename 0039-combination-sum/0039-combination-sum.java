ac58abd7449db473f5ed055341fbb8983f9c5ad5
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-15 11:55:25 +0530

    Time: 8 ms (7.96%), Space: 43.8 MB (76.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,54 +1,54 @@
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, candidates, target, ans, new Stack<>());
-        return ans;
-    }
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         findCombinations(0, candidates, target, ans, new Stack<>());
+//         return ans;
+//     }
 
-    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, Stack<Integer> ds) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
+//     private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, Stack<Integer> ds) {
+//         if (target == 0) {
+//             ans.add(new ArrayList<>(ds));
+//             return;
+//         }
 
-        if (ind == arr.length || target < 0) {
-            return;
-        }
+//         if (ind == arr.length || target < 0) {
+//             return;
+//         }
 
-        // Choice 1: Take the current element
-        if (arr[ind] <= target) {
-            ds.push(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
-            ds.pop();
-        }
+//         // Choice 1: Take the current element
+//         if (arr[ind] <= target) {
+//             ds.push(arr[ind]);
+//             findCombinations(ind, arr, target - arr[ind], ans, ds);
+//             ds.pop();
+//         }
 
-        // Choice 2: Don't take the current element
-        findCombinations(ind + 1, arr, target, ans, ds);
-    }
-}
+//         // Choice 2: Don't take the current element
+//         findCombinations(ind + 1, arr, target, ans, ds);
+//     }
+// }
 
 // // MEHTOD 1 : SubSequence 
 
-// class Solution {
-//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//         List<List<Integer>> subset = new ArrayList<>();
-//         Stack<Integer> curset = new Stack<>();
-//         helper(0, target, candidates, curset, subset);
-//         return subset;
-//     }
-//     static void helper(int i, int target, int[] candidates, Stack<Integer> curset, List<List<Integer>> subset) {
-//         if (i == candidates.length || target < 0) {
-//             if (target == 0) subset.add(new ArrayList<>(curset));
-//             return;
-//         }
-//         // choice 1 : Take
-//         curset.push(candidates[i]);
-//         helper(i, target - candidates[i], candidates, curset, subset);
-//         //choice 2 : Don't Take
-//         curset.pop();
-//         helper(i + 1, target, candidates, curset, subset);
-//     }
-// }
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> subset = new ArrayList<>();
+        Stack<Integer> curset = new Stack<>();
+        helper(0, target, candidates, curset, subset);
+        return subset;
+    }
+    static void helper(int i, int target, int[] candidates, Stack<Integer> curset, List<List<Integer>> subset) {
+        if (i == candidates.length || target < 0) {
+            if (target == 0) subset.add(new ArrayList<>(curset));
+            return;
+        }
+        // choice 1 : Take
+        curset.push(candidates[i]);
+        helper(i, target - candidates[i], candidates, curset, subset);
+        //choice 2 : Don't Take
+        curset.pop();
+        helper(i + 1, target, candidates, curset, subset);
+    }
+}
 
 // METHOD 2 : C(n,k) 
 //
