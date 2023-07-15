212c8f59c3bb6f71da8fd580c66ab0897cff947f
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-15 11:10:53 +0530

    Time: 1 ms (100.00%), Space: 43.8 MB (83.37%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,3 +1,34 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        if (ind == arr.length || target < 0) {
+            return;
+        }
+
+        // Choice 1: Take the current element
+        if (arr[ind] <= target) {
+            ds.add(arr[ind]);
+            findCombinations(ind, arr, target - arr[ind], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+
+        // Choice 2: Don't take the current element
+        findCombinations(ind + 1, arr, target, ans, ds);
+    }
+}
+
+// // MEHTOD 1 : SubSequence 
+
 // class Solution {
 //     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 //         List<List<Integer>> subset = new ArrayList<>();
@@ -18,59 +49,29 @@
 //         helper(i + 1, target, candidates, curset, subset);
 //     }
 // }
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> subset = new ArrayList<>();
-        Stack<Integer> curset = new Stack<>();
-        helper(0, candidates, target, curset, subset);
-        return subset;
-    }
 
-    static void helper(int i, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
-        if (target == 0) {
-            subset.add(new ArrayList<>(curset));
-            return;
-        }
-        if (target < 0)
-            return;
-        for (int j = i; j < candidates.length; j++) {
-            curset.push(candidates[j]);
-            target -= candidates[j];
-            helper(j, candidates, target, curset, subset);
-            target += curset.pop(); // sum + element removed
-        }
-    }
-}
-
-// METHOD 1:
-// static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
-//     if (sum == target) {
-//         subset.add(new ArrayList<>(curset));
-//         return;
+// METHOD 2 : C(n,k) 
+//
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> subset = new ArrayList<>();
+//         Stack<Integer> curset = new Stack<>();
+//         helper(0, candidates, target, curset, subset);
+//         return subset;
 //     }
-//     if (sum > target || i == candidates.length)
-//         return;
-//     // choice -> take i
-//     curset.push(candidates[i]);
-//     sum += candidates[i];
-//     helper(i, sum, candidates, target, curset, subset);
-//     sum -= curset.pop();
-//     // choice -> don't take
-//     helper(i + 1, sum, candidates, target, curset, subset);
-// }
 
-// METHOD 2
-// static void helper(int i, int sum, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
-//     if (sum == target) {
-//         subset.add(new ArrayList<>(curset));
-//         return;
-//     }
-//     if (sum > target)
-//         return;
-//     for (int j = i; j < candidates.length; j++) {
-//         curset.push(candidates[j]);
-//         sum += candidates[j];
-//         helper(j, sum, candidates, target, curset, subset);
-//         sum -= curset.pop(); // sum + element removed
+//     static void helper(int i, int[] candidates, int target, Stack<Integer> curset, List<List<Integer>> subset) {
+//         if (target == 0) {
+//             subset.add(new ArrayList<>(curset));
+//             return;
+//         }
+//         if (target < 0)
+//             return;
+//         for (int j = i; j < candidates.length; j++) {
+//             curset.push(candidates[j]);
+//             target -= candidates[j];
+//             helper(j, candidates, target, curset, subset);
+//             target += curset.pop(); // sum + element removed
+//         }
 //     }
 // }
