d1a46f5391fefbd661ef9aacf7d2eec7836e5acd
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-15 12:30:34 +0530

    Time: 2 ms (81.44%), Space: 43.5 MB (92.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,35 +1,3 @@
-// // CORRECT
-// class Solution {
-//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//         List<List<Integer>> ans = new ArrayList<>();
-//         findCombinations(0, candidates, target, ans, new ArrayList<>());
-//         return ans;
-//     }
-
-//     private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-//         if (target == 0) {
-//             ans.add(new ArrayList<>(ds));
-//             return;
-//         }
-
-//         if (ind == arr.length || target < 0) {
-//             return;
-//         }
-
-//         // Choice 1: Take the current element
-//         if (arr[ind] <= target) {
-//             ds.add(arr[ind]);
-//             findCombinations(ind, arr, target - arr[ind], ans, ds);
-//             ds.remove(ds.size() - 1);
-//         }
-
-//         // Choice 2: Don't take the current element
-//         findCombinations(ind + 1, arr, target, ans, ds);
-//     }
-// }
-
-
-
 // MEHTOD 1 : SubSequence 
 
 class Solution {
@@ -45,15 +13,16 @@ class Solution {
             return;
         }
         // choice 1 : Take
-        if (candidates[i] <= target) {
-        curset.add(candidates[i]);
-        helper(i, target - candidates[i], candidates, curset, subset);
-        //choice 2 : Don't Take
-        curset.remove(curset.size() - 1);
+        if (candidates[i] <= target) { // ***
+            curset.add(candidates[i]);
+            helper(i, target - candidates[i], candidates, curset, subset);
+            curset.remove(curset.size() - 1);
         }
+        //choice 2 : Don't Take
         helper(i + 1, target, candidates, curset, subset);
     }
 }
+
 
 // METHOD 2 : C(n,k) 
 //
