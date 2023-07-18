848bafb9f1f32edac823a08fba86094eccd050af
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-18 11:32:57 +0530

    Time: 3 ms (66.11%), Space: 42.4 MB (72.12%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> subset = new ArrayList<>();
+        List<Integer> curset = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(0, target, candidates, curset, subset);
+        return subset;
+    }
+    static void helper(int i, int target, int[] candidates, List<Integer> curset, List<List<Integer>> subset) {
+        if (target == 0) {
+            subset.add(new ArrayList<>(curset));
+            return;
+        }
+        if (i == candidates.length) return;
+        
+        //TAKE ALL DUPLICATES
+        if (candidates[i] <= target) {
+            curset.add(candidates[i]);
+            helper(i + 1, target - candidates[i], candidates, curset, subset);
+            curset.remove(curset.size() - 1);
+        }
+        while (i + 1 < candidates.length && candidates[i] == candidates[i + 1]) i++; //at last duplicate
+        helper(i + 1, target, candidates, curset, subset);
+    }
+}
