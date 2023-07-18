8b621fed8fe8643a91dd6e442ef58cdd00db5c93
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-18 14:03:06 +0530

    Time: 2 ms (99.72%), Space: 42.7 MB (43.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -11,17 +11,45 @@ class Solution {
             subset.add(new ArrayList<>(curset));
             return;
         }
-        if (i == candidates.length || candidates[i] > target) return;
-        
-        //TAKE ALL DUPLICATES
-        // if (candidates[i] <= target) {
-            curset.add(candidates[i]);
-            helper(i + 1, target - candidates[i], candidates, curset, subset);
-            curset.remove(curset.size() - 1);
-        // }
-        // else return;
+        if (i == candidates.length || candidates[i] > target) return; //since all ahead 'i' will be greater than target
+        // TAKE ALL DUPLICATES
+        curset.add(candidates[i]);
+        helper(i + 1, target - candidates[i], candidates, curset, subset);
+        curset.remove(curset.size() - 1);
         //SKIP ALL DUPLICATES
         while (i + 1 < candidates.length && candidates[i] == candidates[i + 1]) i++; //at last duplicate
         helper(i + 1, target, candidates, curset, subset);
     }
 }
+
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//         List<List<Integer>> subset = new ArrayList<>();
+//         List<Integer> curset = new ArrayList<>();
+//         Arrays.sort(candidates);
+//         helper(0, target, candidates, curset, subset);
+//         return subset;
+//     }
+//     static void helper(int i, int target, int[] candidates, List<Integer> curset, List<List<Integer>> subset) {
+//         if (target == 0) {  //*** COND. ABOVE since MUST TAKE LAST ELEMENT
+//             subset.add(new ArrayList<>(curset));
+//             return;
+//         }
+//         if (i == candidates.length || candidates[i] > target) return;
+        
+//         //TAKE ALL DUPLICATES
+//         // if (candidates[i] <= target) {
+//             curset.add(candidates[i]);
+//             helper(i + 1, target - candidates[i], candidates, curset, subset);
+//             curset.remove(curset.size() - 1);
+//         // }
+//         // else return;
+//         //SKIP ALL DUPLICATES
+//         while (i + 1 < candidates.length && candidates[i] == candidates[i + 1]) i++; //at last duplicate
+//         helper(i + 1, target, candidates, curset, subset);
+//     }
+// }
