7a3f2b0947124c025fdf2f9463340acabaa8433a
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-18 10:23:18 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (52.70%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,48 +1,27 @@
+// MEHTOD 1 : SubSequence 
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> subset = new ArrayList<>();
-        helper(0, target, candidates, new ArrayList<>(), subset);
+        List<Integer> curset = new ArrayList<>();
+        helper(0, target, candidates, curset, subset);
         return subset;
     }
     static void helper(int i, int target, int[] candidates, List<Integer> curset, List<List<Integer>> subset) {
-        if (i == candidates.length) return;
-        if (target == 0) {
-            subset.add(new ArrayList<>(curset));
+        if (i == candidates.length || target < 0) {
+            if (target == 0) subset.add(new ArrayList<>(curset));
             return;
         }
-        //choice 1 : Take
-        if (candidates[i] <= target) {  //prevents additional recursrion + backtracking [which takes considerable time]
+        // choice 1 : Take
+        if (candidates[i] <= target) { // *** prevents additional recursrion + backtracking [which takes considerable time]
             curset.add(candidates[i]);
             helper(i, target - candidates[i], candidates, curset, subset);
             curset.remove(curset.size() - 1);
         }
+        //choice 2 : Don't Take
         helper(i + 1, target, candidates, curset, subset);
     }
 }
-// // MEHTOD 1 : SubSequence 
-
-// class Solution {
-//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-//         List<List<Integer>> subset = new ArrayList<>();
-//         List<Integer> curset = new ArrayList<>();
-//         helper(0, target, candidates, curset, subset);
-//         return subset;
-//     }
-//     static void helper(int i, int target, int[] candidates, List<Integer> curset, List<List<Integer>> subset) {
-//         if (i == candidates.length || target < 0) {
-//             if (target == 0) subset.add(new ArrayList<>(curset));
-//             return;
-//         }
-//         // choice 1 : Take
-//         if (candidates[i] <= target) { // *** Prevents unnnecessary recursive calls and backtracking
-//             curset.add(candidates[i]);
-//             helper(i, target - candidates[i], candidates, curset, subset);
-//             curset.remove(curset.size() - 1);
-//         }
-//         //choice 2 : Don't Take
-//         helper(i + 1, target, candidates, curset, subset);
-//     }
-// }
 
 
 // // METHOD 2 : C(n,k) 
