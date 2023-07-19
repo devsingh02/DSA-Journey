f22a3e240f3efd77ce1562b03ca31a68d46814cc
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 09:07:25 +0530

    Time: 3 ms (10.45%), Space: 43.8 MB (85.43%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,3 +1,5 @@
+// METHOD 1 : Kunal / String
+
 // class Solution {
 //     public List<List<Integer>> permute(int[] nums) {
 //         List<List<Integer>> ans = new ArrayList<>();
@@ -27,6 +29,32 @@
 //         }
 //     }
 // }
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        permutation(new ArrayList<>(), nums, ans);
+        return ans;
+    }
+
+    static void permutation(List<Integer> p, int[] nums, List<List<Integer>> ans) {
+        if (p.size() == nums.length) {
+            ans.add(new ArrayList<>(p));
+            return;
+        }
+
+        for (int ch : nums) {
+            if (!p.contains(ch)) {
+                List<Integer> newp = new ArrayList<>(p);
+                newp.add(ch);
+                permutation(newp, nums, ans);
+            }
+        }
+    }
+}
+
+
+// // METHOD 2 : SWAPPING
 
 // class Solution {
 //     private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
@@ -59,33 +87,4 @@
 //         return ans;
 //     }
 // }
-class Solution {
-    private void backtrack(int index, int[] nums, List<List<Integer>> ans) {
-        if (index == nums.length) {
-            List<Integer> permutation = new ArrayList<>();
-            for (int num : nums) {
-                permutation.add(num);
-            }
-            ans.add(permutation);
-            return;
-        }
 
-        for (int i = index; i < nums.length; i++) {
-            swap(nums, i, index);
-            backtrack(index + 1, nums, ans);
-            swap(nums, i, index);
-        }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        backtrack(0, nums, ans);
-        return ans;
-    }
-}
