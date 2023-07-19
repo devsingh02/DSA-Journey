02ee03a7512e61e52c4c71dab5b0015dc8c48d38
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 10:25:15 +0530

    Time: 4 ms (61.55%), Space: 44 MB (75.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -14,45 +14,37 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        generatePermutations(new ArrayList<>(), n, ans);
+        generatePermutations(new ArrayList<>(), new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1], n, ans);
         return ans;
     }
-static void generatePermutations(List<String> current, int n, List<List<String>> permutations) {
+static void generatePermutations(List<String> current, boolean[] cols, boolean[] diagonal1, boolean[] diagonal2, int n, List<List<String>> permutations) {
         if (current.size() == n) {
             permutations.add(new ArrayList<>(current));
             return;
         }
-         for (int i = 0; i < n; i++) {
-            if (isValid(current, i)) {
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < n; j++) {
-                    if (j == i) {
+         int row = current.size();
+        for (int col = 0; col < n; col++) {
+            int diag1 = row - col + n - 1;
+            int diag2 = row + col;
+             if (!cols[col] && !diagonal1[diag1] && !diagonal2[diag2]) {
+                cols[col] = true;
+                diagonal1[diag1] = true;
+                diagonal2[diag2] = true;
+                 StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < n; i++) {
+                    if (i == col) {
                         sb.append("Q");
                     } else {
                         sb.append(".");
                     }
                 }
                 current.add(sb.toString());
-                generatePermutations(current, n, permutations);
-                current.remove(current.size() - 1);
+                 generatePermutations(current, cols, diagonal1, diagonal2, n, permutations);
+                 current.remove(current.size() - 1);
+                cols[col] = false;
+                diagonal1[diag1] = false;
+                diagonal2[diag2] = false;
             }
         }
-    }
-     static boolean isValid(List<String> current, int col) {
-        int row = current.size();
-        for (int i = 0; i < row; i++) {
-            String rowStr = current.get(i);
-            if (rowStr.charAt(col) == 'Q') {
-                return false;
-            }
-            int diff = row - i;
-            if (col - diff >= 0 && rowStr.charAt(col - diff) == 'Q') {
-                return false;
-            }
-            if (col + diff < rowStr.length() && rowStr.charAt(col + diff) == 'Q') {
-                return false;
-            }
-        }
-        return true;
     }
 }
