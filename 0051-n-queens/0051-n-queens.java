4f6e40a5489da359e4d301b6b76b2269baa1a81d
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 20:54:40 +0530

    Time: 2 ms (91.04%), Space: 43.7 MB (92.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,3 +1,63 @@
+
+ class Solution {
+     static List<List<String>> solveNQueens(int n) {
+        // Initialize the chess board with empty cells represented by '.'
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = '.';
+            }
+        }
+        // Initialize lists to store the solutions
+        List<List<String>> res = new ArrayList<>();
+        // Initialize arrays to keep track of used columns and diagonals
+        int[] usedCols = new int[n];
+        int[] usedLowerDiagonal = new int[2 * n - 1];
+        int[] usedUpperDiagonal = new int[2 * n - 1];
+        // Call the recursive solve method
+        solve(0, board, res, usedCols, usedLowerDiagonal, usedUpperDiagonal);
+        // Return the solutions
+        return res;
+    }
+     // Recursive function to solve the N-Queens problem
+    static void solve(int row, char[][] board, List<List<String>> res, int[] usedCols, int[] usedLowerDiagonal, int[] usedUpperDiagonal){
+        // If all rows are filled with queens, add the solution to the list and return
+        if (row == board.length){
+            res.add(construct(board));
+            return;
+        }
+        // Try placing a queen in each column of the current row
+        for (int col = 0; col < board.length; col++){
+            // Check if the current cell is safe for a queen
+            if(usedCols[col] == 0 && usedLowerDiagonal[col + row] == 0 && usedUpperDiagonal[board.length - 1 + row - col] == 0){
+                // Place the queen in the current cell
+                board[row][col] = 'Q';
+                // Mark the used column and diagonals
+                usedCols[col] = 1;
+                usedLowerDiagonal[col + row] = 1;
+                usedUpperDiagonal[board.length - 1 + row - col] = 1;
+                // Recursively solve for the next row
+                solve(row + 1, board, res, usedCols, usedLowerDiagonal, usedUpperDiagonal);
+                // Remove the queen from the current cell
+                board[row][col] = '.';
+                // Unmark the used column and diagonals
+                usedCols[col] = 0;
+                usedLowerDiagonal[col + row] = 0;
+                usedUpperDiagonal[board.length - 1 + row - col] = 0;
+            }
+        }
+    }
+    // Helper function to construct a list of strings representing the chess board
+    static List<String> construct(char board[][]){
+        List<String> res = new LinkedList<>();
+        for(int i = 0; i < board.length; i++){
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
+
 // // METHOD 1 : Most efficient
 // class Solution {
 
@@ -78,53 +138,53 @@
 //     }
 // }
 
- // METHOD 2 : Kunal's / Default
+//  // METHOD 2 : Kunal's / Default
 
-class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        generatePermutations(new ArrayList<>(), n, ans);
-        return ans;
-    }
-    static void generatePermutations(List<String> current, int n, List<List<String>> permutations) {
-        if (current.size() == n) {
-            permutations.add(new ArrayList<>(current));
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (isValid(current, i)) {
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < n; j++) {
-                    if (j == i) {
-                        sb.append("Q");
-                    } else {
-                        sb.append(".");
-                    }
-                }
-                current.add(sb.toString());
-                generatePermutations(current, n, permutations);
-                current.remove(current.size() - 1);
-            }
-        }
-    }
-     static boolean isValid(List<String> current, int col) {
-        int row = current.size();
-        for (int i = 0; i < row; i++) {
-            String rowStr = current.get(i);
-            if (rowStr.charAt(col) == 'Q') {
-                return false;
-            }
-            int diff = row - i;
-            if (col - diff >= 0 && rowStr.charAt(col - diff) == 'Q') {
-                return false;
-            }
-            if (col + diff < rowStr.length() && rowStr.charAt(col + diff) == 'Q') {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+// class Solution {
+//     public List<List<String>> solveNQueens(int n) {
+//         List<List<String>> ans = new ArrayList<>();
+//         generatePermutations(new ArrayList<>(), n, ans);
+//         return ans;
+//     }
+//     static void generatePermutations(List<String> current, int n, List<List<String>> permutations) {
+//         if (current.size() == n) {
+//             permutations.add(new ArrayList<>(current));
+//             return;
+//         }
+//         for (int i = 0; i < n; i++) {
+//             if (isValid(current, i)) {
+//                 StringBuilder sb = new StringBuilder();
+//                 for (int j = 0; j < n; j++) {
+//                     if (j == i) {
+//                         sb.append("Q");
+//                     } else {
+//                         sb.append(".");
+//                     }
+//                 }
+//                 current.add(sb.toString());
+//                 generatePermutations(current, n, permutations);
+//                 current.remove(current.size() - 1);
+//             }
+//         }
+//     }
+//      static boolean isValid(List<String> current, int col) {
+//         int row = current.size();
+//         for (int i = 0; i < row; i++) {
+//             String rowStr = current.get(i);
+//             if (rowStr.charAt(col) == 'Q') {
+//                 return false;
+//             }
+//             int diff = row - i;
+//             if (col - diff >= 0 && rowStr.charAt(col - diff) == 'Q') {
+//                 return false;
+//             }
+//             if (col + diff < rowStr.length() && rowStr.charAt(col + diff) == 'Q') {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
 
 
 
