079421b94b75efb8d93497a4c249b589cf154345
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 10:20:59 +0530

    Time: 9 ms (19.98%), Space: 44.2 MB (47.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -12,78 +12,47 @@
 //     }
 // }
 class Solution {
-    List<List<String>> list = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        char[][] matrix = new char[n][n];
-        //initally fill all cells by empty charatcer
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[i][j] = '.';
-            }
-        }
-        //make back tracking function call
-        func(matrix, 0);
-        return list;
+        List<List<String>> ans = new ArrayList<>();
+        generatePermutations(new ArrayList<>(), n, ans);
+        return ans;
     }
-    
-    
-    
-    public void func(char[][] matrix, int x){
-        //base case
-        if(x == matrix.length){ 
-            // when all queens are settled, save this matrix to final List
-            ArrayList<String> ll = new ArrayList<>();
-            for(int i=0;i<matrix.length;i++){
-                String str = "";
-                for(int j=0;j<matrix[0].length;j++){
-                    if(matrix[i][j] == 'Q'){
-                        str+="Q";
-                    }
-                    else{
-                        str+=".";
-                    }
-                }
-                ll.add(str);
-            }
-            list.add(new ArrayList(ll));
+static void generatePermutations(List<String> current, int n, List<List<String>> permutations) {
+        if (current.size() == n) {
+            permutations.add(new ArrayList<>(current));
             return;
         }
-        
-        //recursion call for all columns at a row
-        for(int i=0;i<matrix[0].length;i++){
-            if(isSafe(matrix, x, i)){    // check if possible to put queen
-                matrix[x][i] = 'Q'; // put queen
-                func(matrix, x+1);  //do for next row
-                matrix[x][i] = '.'; // remove queen
+         for (int i = 0; i < n; i++) {
+            if (isValid(current, i)) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < n; j++) {
+                    if (j == i) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                current.add(sb.toString());
+                generatePermutations(current, n, permutations);
+                current.remove(current.size() - 1);
             }
         }
-        // matrix[x][j] = '*';
     }
-    
-
-    
-    public boolean isSafe(char[][] matrix, int x,int y){
-        //vertical
-        for(int i=x-1;i>=0;i--){
-            if(matrix[i][y] == 'Q'){
+     static boolean isValid(List<String> current, int col) {
+        int row = current.size();
+        for (int i = 0; i < row; i++) {
+            String rowStr = current.get(i);
+            if (rowStr.charAt(col) == 'Q') {
                 return false;
             }
-        }
-        
-        //left diagonal
-        for(int i=x-1,j=y-1;i>=0 && j>=0 ; i--,j--){
-            if(matrix[i][j] == 'Q'){
+            int diff = row - i;
+            if (col - diff >= 0 && rowStr.charAt(col - diff) == 'Q') {
                 return false;
             }
-        }
-        
-        //right diagonal
-        for(int i=x-1,j=y+1;i >=0 && j < matrix[0].length; i--, j++){
-            if(matrix[i][j] == 'Q'){
+            if (col + diff < rowStr.length() && rowStr.charAt(col + diff) == 'Q') {
                 return false;
             }
         }
         return true;
     }
-    
 }
