b7c4b2b469881a55c0b451a88122ac8f3c7f2446
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 09:30:05 +0530

    Time: 1 ms (98.85%), Space: 43.6 MB (91.93%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,75 +1,76 @@
 // // METHOD 1 : Kunal / String
 
-// class Solution {
-//     public List<List<Integer>> permute(int[] nums) {
-//         List<List<Integer>> ans = new ArrayList<>();
-//         permutation(new ArrayList<>(), 0, nums, ans);
-//         return ans;
-//     }
-//     static void permutation(List<Integer> p, int ind, int[] nums, List<List<Integer>> ans) {
-//         if (ind == nums.length) {
-//             ans.add(new ArrayList<>(p));
-//             return;
-//         }
-//         int ch = nums[ind];
-//         for (int i = 0; i <= p.size(); i++) {
-//             // List<Integer> f = new ArrayList<>(); for(int b = 0; b < a; b++) f.add(nums[b]);
-//             // List<Integer> l = new ArrayList<>(); for(int b = a; b < p.size(); b++) l.add(nums[b]);
-//             List<Integer> newp = new ArrayList<>();
-//             for (int k = 0; k < p.size(); k++) {
-//                 if (k == i) {
-//                     newp.add(ch);
-//                     newp.add(p.get(k));
-//                     continue;
-//                 }
-//                 newp.add(p.get(k));
-//             }
-//             if (i == p.size()) newp.add(ch);
-//             permutation(newp, ind + 1, nums, ans);
-//         }
-//     }
-// }
 class Solution {
-    
-public static void solve(int[] nums,List<List<Integer>>v,int i)
-    {
-        if(i>=nums.length)
-        {
-             ArrayList<Integer> cl = new ArrayList<>();
-			//creating the list every time when the base conditions get hit
-            for(int x:nums)
-            {
-                cl.add(x);
-            }
-            v.add(cl);
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        permutation(new ArrayList<>(), 0, nums, ans);
+        return ans;
+    }
+    static void permutation(List<Integer> p, int ind, int[] nums, List<List<Integer>> ans) {
+        if (ind == nums.length) {
+            ans.add(new ArrayList<>(p));
             return;
         }
-        for(int j=i;j<nums.length;j++)
-        {
-            
-            //swap 
-    int temp=nums[i];
-    nums[i]=nums[j];
-    nums[j]=temp;
-        
-            solve(nums,v,i+1);
-            
-            
-     //swap
-     temp=nums[i];
-    nums[i]=nums[j];
-    nums[j]=temp;
+        int ch = nums[ind];
+        for (int i = 0; i <= p.size(); i++) {
+            List<Integer> newp = new ArrayList<>(p);
+            newp.add(i, ch);
+            // for (int k = 0; k < p.size(); k++) {
+            //     if (k == i) {
+            //         newp.add(ch);
+            //         newp.add(p.get(k));
+            //         continue;
+            //     }
+            //     newp.add(p.get(k));
+            // }
+            // if (i == p.size()) newp.add(ch);
+            permutation(newp, ind + 1, nums, ans);
         }
-          
-    }
-    public List<List<Integer>> permute(int[] nums) {
-         List<List<Integer>>v=new ArrayList<>();
-         int i=0;
-        solve(nums,v,i);
-        return v;
-        
     }
 }
+
+
+// class Solution {
+    
+// public static void solve(int[] nums,List<List<Integer>>v,int i)
+//     {
+//         if(i>=nums.length)
+//         {
+//              ArrayList<Integer> cl = new ArrayList<>();
+// 			//creating the list every time when the base conditions get hit
+//             for(int x:nums)
+//             {
+//                 cl.add(x);
+//             }
+//             v.add(cl);
+//             return;
+//         }
+//         for(int j=i;j<nums.length;j++)
+//         {
+            
+//             //swap 
+//     int temp=nums[i];
+//     nums[i]=nums[j];
+//     nums[j]=temp;
+        
+//             solve(nums,v,i+1);
+            
+            
+//      //swap
+//      temp=nums[i];
+//     nums[i]=nums[j];
+//     nums[j]=temp;
+//         }
+          
+//     }
+//     public List<List<Integer>> permute(int[] nums) {
+//          List<List<Integer>>v=new ArrayList<>();
+//          int i=0;
+//         solve(nums,v,i);
+//         return v;
+        
+//     }
+// }
 
 // // METHOD 2 : SWAPPING
 
