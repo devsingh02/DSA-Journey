9ba32c1abbc5d267314499c2ecc0832bb96dd023
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 18:44:09 +0530

    Time: 8 ms (26.25%), Space: 44.2 MB (47.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,69 +1,48 @@
 class Solution {
-    
-    public List < List < String >> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List < List < String >> res = new ArrayList < List < String >> ();
-        dfs(0, board, res);
-        return res;
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        generatePermutations(new ArrayList<>(), n, ans);
+        return ans;
     }
-
-    static boolean validate(char[][] board, int row, int col) {
-        int duprow = row;
-        int dupcol = col;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0 && row < board.length) {
-            if (board[row][col] == 'Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-
-    static void dfs(int col, char[][] board, List < List < String >> res) {
-        if (col == board.length) {
-            res.add(construct(board));
+    static void generatePermutations(List<String> current, int n, List<List<String>> permutations) {
+        if (current.size() == n) {
+            permutations.add(new ArrayList<>(current));
             return;
         }
-
-        for (int row = 0; row < board.length; row++) {
-            if (validate(board, row, col)) {
-                board[row][col] = 'Q';
-                dfs(col + 1, board, res);
-                board[row][col] = '.';
+        for (int i = 0; i < n; i++) {
+            if (isValid(current, i)) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < n; j++) {
+                    if (j == i) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                current.add(sb.toString());
+                generatePermutations(current, n, permutations);
+                current.remove(current.size() - 1);
             }
         }
     }
-
-
-
-    static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
+     static boolean isValid(List<String> current, int col) {
+        int row = current.size();
+        for (int i = 0; i < row; i++) {
+            String rowStr = current.get(i);
+            if (rowStr.charAt(col) == 'Q') {
+                return false;
+            }
+            int diff = row - i;
+            if (col - diff >= 0 && rowStr.charAt(col - diff) == 'Q') {
+                return false;
+            }
+            if (col + diff < rowStr.length() && rowStr.charAt(col + diff) == 'Q') {
+                return false;
+            }
         }
-        return res;
+        return true;
     }
 }
-
-
 // class Solution {
 
 //     public List<List<String>> solveNQueens(int n) {
