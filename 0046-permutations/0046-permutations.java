23d4ea5d434da3ff2abbf8daf93dc3659eb1cda9
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-19 08:43:35 +0530

    Time: 2 ms (48.01%), Space: 44.3 MB (5.02%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,55 +1,82 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(new ArrayList<>(), ans, nums);
+        generatePermutations(new ArrayList<>(), nums, ans);
         return ans;
     }
-    
-    public void backtrack(List<Integer> curr, List<List<Integer>> ans, int[] nums) {
-        if (curr.size() == nums.length) {
-            ans.add(new ArrayList<>(curr));
+    static void generatePermutations(List<Integer> current, int[] nums, List<List<Integer>> permutations) {
+        if (current.size() == nums.length) {
+            permutations.add(new ArrayList<>(current));
             return;
         }
-        
-        for (int num: nums) {
-            if (!curr.contains(num)) {
-                curr.add(num);
-                backtrack(curr, ans, nums);
-                curr.remove(curr.size() - 1);
+         for (int i = 0; i < nums.length; i++) {
+            if (current.contains(nums[i])) {
+                continue;
             }
+            current.add(nums[i]);
+            generatePermutations(current, nums, permutations);
+            current.remove(current.size() - 1);
         }
     }
 }
 
 
 // class Solution {
-//     private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
-//         if (index == nums.length) {
-//             // copy the ds to ans
-//             List<Integer> ds = new ArrayList<>();
-//             for (int i = 0; i < nums.length; i++) {
-//                 ds.add(nums[i]);
-//             }
-//             ans.add(new ArrayList<>(ds));
-//             return;
-//         }
-
-//         for (int i = index; i < nums.length; i++) {
-//             swap(i, index, nums);
-//             recurPermute(index + 1, nums, ans);
-//             swap(i, index, nums);
-//         }
-//     }
-
-//     private void swap(int i, int j, int[] nums) {
-//         int t = nums[i];
-//         nums[i] = nums[j];
-//         nums[j] = t;
-//     }
-
 //     public List<List<Integer>> permute(int[] nums) {
-//         List<List<Integer>> ans = new ArrayList();
-//         recurPermute(0, nums, ans);
+//         List<List<Integer>> ans = new ArrayList<>();
+//         permutation(new ArrayList<>(), 0, nums, ans);
 //         return ans;
 //     }
+//     static void permutation(List<Integer> p, int ind, int[] nums, List<List<Integer>> ans) {
+//         if (ind == nums.length) {
+//             ans.add(new ArrayList<>(p));
+//             return;
+//         }
+//         int ch = nums[ind];
+//         for (int i = 0; i <= p.size(); i++) {
+//             // List<Integer> f = new ArrayList<>(); for(int b = 0; b < a; b++) f.add(nums[b]);
+//             // List<Integer> l = new ArrayList<>(); for(int b = a; b < p.size(); b++) l.add(nums[b]);
+//             List<Integer> newp = new ArrayList<>();
+//             for (int k = 0; k < p.size(); k++) {
+//                 if (k == i) {
+//                     newp.add(ch);
+//                     k--; continue;
+//                 }
+//                 newp.add(p.get(k));
+//             }
+//             permutation(newp, ind + 1, nums, ans);
+//         }
+//     }
 // }
+
+// // class Solution {
+// //     private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
+// //         if (index == nums.length) {
+// //             // copy the ds to ans
+// //             List<Integer> ds = new ArrayList<>();
+// //             for (int i = 0; i < nums.length; i++) {
+// //                 ds.add(nums[i]);
+// //             }
+// //             ans.add(new ArrayList<>(ds));
+// //             return;
+// //         }
+
+// //         for (int i = index; i < nums.length; i++) {
+// //             swap(i, index, nums);
+// //             recurPermute(index + 1, nums, ans);
+// //             swap(i, index, nums);
+// //         }
+// //     }
+
+// //     private void swap(int i, int j, int[] nums) {
+// //         int t = nums[i];
+// //         nums[i] = nums[j];
+// //         nums[j] = t;
+// //     }
+
+// //     public List<List<Integer>> permute(int[] nums) {
+// //         List<List<Integer>> ans = new ArrayList();
+// //         recurPermute(0, nums, ans);
+// //         return ans;
+// //     }
+// // }
