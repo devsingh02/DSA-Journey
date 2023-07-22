f704a9564e9140b782556c700acb64d1548e382e
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-22 14:25:45 +0530

    Time: 1 ms (99.89%), Space: 39.9 MB (75.30%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -38,63 +38,120 @@
 //         return true;
 //     }
 // }
-class Solution {
-public void solveSudoku(char[][] board) {
-    
-    int[] rows = new int[9];
-    int[] cols = new int[9];
-    int[][] grid = new int[3][3];
-    
-    for(int i=0;i<board.length;i++){
-        for(int j=0;j<board[0].length;j++){
-            rows[i] |= (1<<(board[i][j]-48));
-            cols[j] |= (1<<board[i][j]-48);
-            grid[i/3][j/3] |= (1<<board[i][j]-48);
-        }
-    }
-    SudokuSolver(board,0,0,rows,cols,grid);
-}
 
-public boolean SudokuSolver(char[][] board,int i,int j,int[] rows,int[] cols,int[][] grid){
-    
-    if(i == board.length){
-        return true;
+
+
+
+
+
+
+class Solution {
+
+    private int[] rowRestrictedNumbers;
+    private int[] columnRestrictedNumbers;
+    private int[] squareRestrictedNumbers;
+
+    //[] - row, [] - column
+    public void solveSudoku(char[][] board) {
+        if(board == null)
+            return;
+
+        rowRestrictedNumbers = new int[9];
+        columnRestrictedNumbers = new int[9];
+        squareRestrictedNumbers = new int[9];
+        fillInitialRestrictions(board);
+        sudokuSolverHelper(board);
     }
-    
-    int ni = 0;
-    int nj = 0;
-    
-    if(j == board[0].length-1){
-        ni = i+1;
-        nj = 0;
-    }else{
-        ni = i;
-        nj = j+1;
+
+    private boolean sudokuSolverHelper(char[][] board) {
+        int availableNumbers, ri = 0, ci = 0, count = 0;
+        int minAvailableNumbers = Integer.MAX_VALUE;
+        for(int row = 0 ; row < 9 ; row++) {
+            for (int column = 0; column < 9; column++) {
+                if(board[row][column] == '.') {
+                    availableNumbers = getAvailableNumbersCountForPosition(row, column);
+                    if(minAvailableNumbers > availableNumbers) {
+                        minAvailableNumbers = availableNumbers;
+                        ri = row;
+                        ci = column;
+                    }
+                    count++;
+                }
+            }
+        }
+
+        if(count == 0)
+            return false;
+
+        int arr = getAvailableNumbersForPosition(ri, ci);
+        for(int i = 0; i < 9; i++) {
+            int value = 1 << i;
+            if ((arr & value) != 0)
+               continue;
+
+            board[ri][ci] = (char) (i + '1');
+            if(count == 1)
+                return true;
+
+            setNumberToRestrictedArrays(ri, ci, value);
+            if(sudokuSolverHelper(board))
+                return true;
+
+            removeNumberFromRestrictedArrays(ri, ci, value);
+            board[ri][ci] = '.';
+        }
+        return false;
     }
-    
-    if(board[i][j]!='.'){
-        boolean ans = SudokuSolver(board,ni,nj,rows,cols,grid);
-        if(ans)
-            return true;
-    }else{
-        for(int pos=1;pos<=9;pos++){
-            int mask = 1<<pos;
-            if((rows[i] & mask) == 0 && (cols[j] & mask) == 0 && (grid[i/3][j/3] & mask) == 0){
-                board[i][j] = (char) (pos+48);
-                rows[i]^=mask;
-                cols[j]^=mask;
-                grid[i/3][j/3]^=mask;
-                boolean ans = SudokuSolver(board,ni,nj,rows,cols,grid);
-                if(ans)
-                    return true;
-                board[i][j] = '.';
-                rows[i]^=mask;
-                cols[j]^=mask;
-                grid[i/3][j/3]^=mask;
+
+    private void fillInitialRestrictions(char[][] board){
+        for(int row = 0 ; row < 9 ; row++) {
+            for (int column = 0; column < 9; column++) {
+                if(board[row][column] == '.')
+                    continue;
+                int value = (1 << (board[row][column]-'1'));
+                setNumberToRestrictedArrays(row, column, value);
             }
         }
     }
-        
-    return false;
-}
+
+    private void setNumberToRestrictedArrays(int row, int column, int value) {
+        setNumberToRestricted(rowRestrictedNumbers, row, value);
+        setNumberToRestricted(columnRestrictedNumbers, column, value);
+        setNumberToRestricted(squareRestrictedNumbers, getSquareFromPosition(row, column), value);
+    }
+
+    private void removeNumberFromRestrictedArrays(int row, int column, int value) {
+        removeNumberFromRestricted(rowRestrictedNumbers, row, value);
+        removeNumberFromRestricted(columnRestrictedNumbers, column, value);
+        removeNumberFromRestricted(squareRestrictedNumbers, getSquareFromPosition(row, column), value);
+    }
+
+    //if 1 then restricted;
+    private void setNumberToRestricted(int[] arr, int position, int value) {
+        arr[position] |= value;
+    }
+
+    private void removeNumberFromRestricted(int[] arr, int position, int value) {
+        arr[position] &= ~value;
+    }
+
+    private int getAvailableNumbersForPosition(int row, int column) {
+        int r = rowRestrictedNumbers[row];
+        int c = columnRestrictedNumbers[column];
+        int s = squareRestrictedNumbers[getSquareFromPosition(row, column)];
+        return r | c | s;
+    }
+
+    private int getAvailableNumbersCountForPosition(int row, int column) {
+        int x = getAvailableNumbersForPosition(row, column);
+        int count = 0;
+        for(int i = 0; i < 9; i++) {
+            count += (~(x >> i) & 1);
+        }
+        return count;
+    }
+
+    private int getSquareFromPosition(int row, int column) {
+        return row / 3 * 3 + column / 3;
+    }
 }
