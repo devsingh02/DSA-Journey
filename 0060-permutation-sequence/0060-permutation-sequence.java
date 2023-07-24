b38df02e1804dc52d4ba71b197187ed4a0fae54b
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-24 09:44:18 +0530

    Time: 1 ms (98.40%), Space: 40.1 MB (57.92%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,29 +1,23 @@
 class Solution {
-   	public static String getPermutation(int n, int k) {
-		StringBuilder sb = new StringBuilder("");
-		String rv = "";
-
-		for (int i = 1; i <= n; ++i) {
-			sb.append(i + "");
-		}
-
-		int[] arr = new int[n + 1];
-		arr[0] = arr[1] = 1;
-
-		for (int i = 1; i <= n; ++i) {
-			arr[i] = i * arr[i - 1];
-		}
-
-		while (rv.length() != n) {
-			int index = 0;
-			while (k > arr[sb.length() - 1]) {
-				k -= arr[sb.length() - 1];
-				++index;
-			}
-			rv += sb.charAt(index);
-			sb.deleteCharAt(index);
-		}
-
-		return rv;
-	}
+    public String getPermutation(int n, int k) {
+       StringBuilder res = new StringBuilder();
+       ArrayList<Integer> nums = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            nums.add(i);
+        }
+        k--;
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for(int i = 1; i < n; i++){
+            factorial[i] = factorial[i-1] * i;
+        }
+		
+        while(n>0){
+            int p = k/factorial[n-1];
+            res.append(nums.remove(p));
+            k = k%factorial[n-1];
+            n = n-1; 
+        }
+        return res.toString();
+    }
 }
