d1b151315ce86937867e57a0201645ed322ace75
Devansh Singh <devanshsingh0228@gmail.com>
2023-07-24 09:45:47 +0530

    Time: 2 ms (67.70%), Space: 40.1 MB (67.51%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,23 +1,45 @@
+// class Solution {
+//     public String getPermutation(int n, int k) {
+//        StringBuilder res = new StringBuilder();
+//        ArrayList<Integer> nums = new ArrayList<>();
+//         for(int i = 1; i <= n; i++){
+//             nums.add(i);
+//         }
+//         k--;
+//         int[] factorial = new int[n];
+//         factorial[0] = 1;
+//         for(int i = 1; i < n; i++){
+//             factorial[i] = factorial[i-1] * i;
+//         }
+		
+//         while(n>0){
+//             int p = k/factorial[n-1];
+//             res.append(nums.remove(p));
+//             k = k%factorial[n-1];
+//             n = n-1; 
+//         }
+//         return res.toString();
+//     }
+// }
 class Solution {
     public String getPermutation(int n, int k) {
-       StringBuilder res = new StringBuilder();
-       ArrayList<Integer> nums = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
-            nums.add(i);
+        String ans = "";
+        int fact = 1;
+        List<Integer> temp = new ArrayList<Integer>();
+        for(int i=1;i<n;i++){
+            fact = fact * i;
+            temp.add(i);
         }
-        k--;
-        int[] factorial = new int[n];
-        factorial[0] = 1;
-        for(int i = 1; i < n; i++){
-            factorial[i] = factorial[i-1] * i;
+        temp.add(n);
+        k = k-1;
+        while(true){
+            ans = ans + temp.get(k/fact);
+            temp.remove(k/fact);
+            k = k % fact;
+            if(temp.size()==0) break;
+            fact = fact / temp.size();
+            
         }
-		
-        while(n>0){
-            int p = k/factorial[n-1];
-            res.append(nums.remove(p));
-            k = k%factorial[n-1];
-            n = n-1; 
-        }
-        return res.toString();
+        return ans;
     }
 }
