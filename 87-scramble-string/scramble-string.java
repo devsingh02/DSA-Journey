5da507deeee93e2399932da76a3cdf56d31d3d64
Devansh Singh <devanshsingh0228@gmail.com>
2024-01-08 11:37:13 +0530

    Time: 10 ms (59.24%) | Memory: 44.6 MB (26.97%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -1,64 +1,80 @@
 class Solution {
-    public static boolean isScramble(String s1, String s2) {
-        if (s1.length() != s2.length()) {
+    static HashMap<String, Boolean> mp = new HashMap<String, Boolean>();
+   
+    static boolean isScramble(String S1, String S2)
+    {
+        
+        // Strings of non-equal length
+        // cant' be scramble strings
+        if (S1.length() != S2.length()) {
             return false;
         }
-
-        Map<String, Boolean> memo = new HashMap<>();
-        return isScrambleHelper(s1, s2, memo);
-    }
-
-    private static boolean isScrambleHelper(String s1, String s2, Map<String, Boolean> memo) {
-        String key = s1 + "_" + s2;
-
-        if (memo.containsKey(key)) {
-            return memo.get(key);
+   
+        int n = S1.length();
+   
+        // Empty strings are scramble strings
+        if (n == 0) {
+            return true;
         }
-
-        int length = s1.length();
-
-        if (length == 1) {
-            return s1.equals(s2);
+   
+        // Equal strings are scramble strings
+        if (S1.equals(S2)) {
+            return true;
         }
-
-        // Check if the two strings are anagrams
-        if (!isAnagram(s1, s2)) {
-            memo.put(key, false);
+        // Check for the condition of anagram
+        String copy_S1 = S1, copy_S2 = S2;
+        char[] t1 = copy_S1.toCharArray();
+        char[] t2 = copy_S2.toCharArray();
+        Arrays.sort(t1);
+        Arrays.sort(t2);
+        copy_S1 = new String(t1);
+        copy_S2 = new String(t2);
+   
+        if (!copy_S1.equals(copy_S2)) {
             return false;
         }
-
-        for (int i = 1; i < length; i++) {
-            // Check if the strings are scrambled for the current split
-            if ((isScrambleHelper(s1.substring(0, i), s2.substring(0, i), memo)
-                    && isScrambleHelper(s1.substring(i), s2.substring(i), memo))
-                    || (isScrambleHelper(s1.substring(0, i), s2.substring(length - i), memo)
-                    && isScrambleHelper(s1.substring(i), s2.substring(0, length - i), memo))) {
-                memo.put(key, true);
+   
+        // make key of type string for search in map
+        String key = (S1 + " " + S2);
+        // checking if both string are before calculated or not
+        // if calculated means find in map then return it's
+        // value
+        if (mp.containsKey(key)) {
+            return mp.get(key);
+        }
+   
+        // declaring flag variable to store result
+        boolean flag = false;
+   
+        for (int i = 1; i < n; i++) {
+   
+            // Check if S2[0...i] is a scrambled
+            // string of S1[0...i] and if S2[i+1...n]
+            // is a scrambled string of S1[i+1...n]
+            if (isScramble(S1.substring(0, i), S2.substring(0, i))
+                && isScramble(S1.substring(i, n), S2.substring(i, n))) {
+                flag = true;
+                 mp.put(key, flag);//store for future use
+                return true;
+            }
+   
+            // Check if S2[0...i] is a scrambled
+            // string of S1[n-i...n] and S2[i+1...n]
+            // is a scramble string of S1[0...n-i-1]
+            if (isScramble(S1.substring(0, i), S2.substring(n - i, n))
+                && isScramble(S1.substring(i, n),
+                              S2.substring(0, n - i))) {
+                flag = true;
+                  mp.put(key, flag); //store for future use
                 return true;
             }
         }
-
-        memo.put(key, false);
+   
+        // add key & flag value to map (store for future use)
+        // so next time no required to calculate it again
+        mp.put(key, flag);
+   
+        // If none of the above conditions are satisfied
         return false;
-    }
-
-    private static boolean isAnagram(String s1, String s2) {
-        int[] count = new int[26];
-
-        for (char c : s1.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        for (char c : s2.toCharArray()) {
-            count[c - 'a']--;
-        }
-
-        for (int value : count) {
-            if (value != 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
