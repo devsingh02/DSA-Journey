813cc98476d2337e7db47220695ec019432d1c59
Devansh Singh <devanshsingh0228@gmail.com>
2024-01-08 12:26:13 +0530

    Time: 10 ms (59.24%), Space: 45.2 MB (10.09%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.java
+++ 0087-scramble-string/0087-scramble-string.java
@@ -1,64 +1,40 @@
 class Solution {
-    public static boolean isScramble(String s1, String s2) {
-        if (s1.length() != s2.length()) {
+    static Map<String, Boolean> memo = new HashMap<>();
+    static boolean isAnagram(String s1, String s2) {
+        int[] chars = new int[26]; //0->a 1->b ...
+        for (char ch : s1.toCharArray()) chars[ch-'a']++;
+        for (char ch : s2.toCharArray()) chars[ch-'a']--;
+
+        for (int val : chars) if (val != 0) return false;
+
+        return true;
+    }
+
+    static boolean isScramble(String s1, String s2) {
+        // strings of unequal length for sure never scramble
+        if (s1.length() != s2.length()) return false;
+        // equal strings are always scramble
+        if (s1.equals(s2)) return true;
+
+        String key = s1 + "-" + s2;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        // If a string is not anagram -> it will never be a scramble
+        if (!isAnagram(s1, s2)) {
+            memo.put(key, false);
             return false;
         }
 
-        Map<String, Boolean> memo = new HashMap<>();
-        return isScrambleHelper(s1, s2, memo);
-    }
-
-    private static boolean isScrambleHelper(String s1, String s2, Map<String, Boolean> memo) {
-        String key = s1 + "_" + s2;
-
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        int length = s1.length();
-
-        if (length == 1) {
-            return s1.equals(s2);
-        }
-
-        // // Check if the two strings are anagrams
-        // if (!isAnagram(s1, s2)) {
-        //     memo.put(key, false);
-        //     return false;
-        // }
-
-        for (int i = 1; i < length; i++) {
-            // Check if the strings are scrambled for the current split
-            if ((isScrambleHelper(s1.substring(0, i), s2.substring(0, i), memo)
-                    && isScrambleHelper(s1.substring(i), s2.substring(i), memo))
-                    || (isScrambleHelper(s1.substring(0, i), s2.substring(length - i), memo)
-                    && isScrambleHelper(s1.substring(i), s2.substring(0, length - i), memo))) {
+        int n = s1.length();
+        for (int i = 1; i < n; i++) {   //partitions using substring
+            // swap || no-swap
+            if ((isScramble(s1.substring(0,i),s2.substring(n-i)) && isScramble(s1.substring(i),s2.substring(0,n-i)))
+                    || (isScramble(s1.substring(0,i),s2.substring(0,i)) && isScramble(s1.substring(i),s2.substring(i)))) {
                 memo.put(key, true);
                 return true;
             }
         }
-
         memo.put(key, false);
         return false;
     }
-
-//     private static boolean isAnagram(String s1, String s2) {
-//         int[] count = new int[26];
-
-//         for (char c : s1.toCharArray()) {
-//             count[c - 'a']++;
-//         }
-
-//         for (char c : s2.toCharArray()) {
-//             count[c - 'a']--;
-//         }
-
-//         for (int value : count) {
-//             if (value != 0) {
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
 }
