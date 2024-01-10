aaa7e940c57984cbd5ef89a07055f9e2e7250f53
Devansh Singh <devanshsingh0228@gmail.com>
2024-01-10 15:04:35 +0530

    Time: 167 ms (50.00%) | Memory: 45.3 MB (11.00%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,20 +1,16 @@
 class Solution {
     public static int mod = 1000000007;
 
-    public int numberOfWays(int n, int x) {
-        int nn = (int) Math.round(Math.pow(n, 1.0 / x)); // Use 1.0 to ensure double division
-        // int nn = 1;
-        // while (Math.pow(nn, x) <= n) {
-        //     nn++;
-        // }
-        int sum = n;
-        int[][] memo = new int[nn + 1][sum + 1];
-        for (int i = 0; i <= nn; i++) {
+    public int numberOfWays(int num, int x) {
+        int n = (int) Math.round(Math.pow(num, 1.0 / x)); // Use 1.0 
+        int sum = num;
+        int[][] memo = new int[n + 1][sum + 1];
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= sum; j++) {
                 memo[i][j] = -1;
             }
         }
-        return (solve(nn, sum, x, memo)) % mod;
+        return solve(n, sum, x, memo);
     }
 
     int solve(int n, int sum, int x, int[][] memo) {
