7836d1f459fc41050b775b1b34bfd2ec7f137726
Devansh Singh <devanshsingh0228@gmail.com>
2024-01-10 16:15:59 +0530

    Time: 848 ms (5.03%) | Memory: 152 MB (5.03%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -2,21 +2,21 @@ class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
         int N = strs.length;
         Map<String, Integer> memo = new HashMap<>();
-        return solve(strs, memo, N, m, n) - 1;
+        return solve(strs, memo, N, m, n);
     }
     int solve(String[] str, Map<String,Integer> memo, int n, int M, int N) {
-        if (n==0&&M==0&&N==0) return 1;
+        if (n==0&&M==0&&N==0) return 0;
         if (M < 0 || N < 0) return 0;
-        if (n == 0) return 1;
+        if (n == 0) return 0;
         String key = n + " " + M + "-" + N;
         if (memo.containsKey(key)) return memo.get(key);
 
         int zeros = zeros(str[n-1]);
         int ones = ones(str[n-1]);
         int ans;
-        // if (zeros <= M && ones <= N)
+        if (zeros <= M && ones <= N)
             ans = Math.max(1 + solve(str, memo, n-1, M-zeros, N-ones), solve(str, memo, n-1, M, N));
-        // else ans = solve(str, memo, n-1, M, N);
+        else ans = solve(str, memo, n-1, M, N);
 
         memo.put(key, ans);
         return ans;
